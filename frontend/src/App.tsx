0735cac1554b08ae30f7cf3232d0b10cf9bc9b70
Rakhshan <rakhshancoder@gmail.com>
2024-09-04 03:56:19 +0530

    removed protected routes for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import Signup from "./pages/Signup"
 import Signin from "./pages/Signin"
 import MyForms from "./pages/MyForms"
 import Appbar from "./components/Appbar"
-import ProtectedRoute from "./components/ProtectedRoute"
+// import ProtectedRoute from "./components/ProtectedRoute"
 
 function App() {
 
@@ -13,8 +13,9 @@ function App() {
       <BrowserRouter>
         <Appbar />
         <Routes>
-          <Route path="/" element={<ProtectedRoute><Form /></ProtectedRoute>} />
-          <Route path="/my-forms" element={<ProtectedRoute><MyForms /></ProtectedRoute>} />
+          {/* <Route path="/" element={<ProtectedRoute><Form /></ProtectedRoute>} /> */}
+          <Route path="/" element={<Form />} />
+          <Route path="/my-forms" element={<MyForms />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
         </Routes>
