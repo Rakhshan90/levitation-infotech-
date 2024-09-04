37a47c3132b627105cf7c84691035730b7e642f6
Rakhshan <rakhshancoder@gmail.com>
2024-09-04 18:38:52 +0530

    Now Routes are protected

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,43 +1,19 @@
-// import React from 'react'
-
 import { Link } from "react-router-dom"
 import { Button } from "./ui/button"
 import { backendURL } from "@/lib/backendURL";
 import axios from "axios";
-import { useEffect, useState } from "react";
 
 const Appbar = () => {
-
-    const [token, setToken] = useState<string | null>(null);
-
-    // Function to get the current token from cookies
-    const getTokenFromCookies = () => {
-        const cookie = document.cookie
-            .split("; ")
-            .find((row) => row.startsWith("token="));
-        return cookie ? cookie.split("=")[1] : null;
-    };
 
     // Function to handle sign out
     const clickHandler = async () => {
         try {
             await axios.post(`${backendURL}/api/user/signout`, {}, { withCredentials: true });
-            setToken(null); // Update token state after sign out
         } catch (error: any) {
             console.log(error);
         }
     };
 
-    // Effect to update the token state when the component mounts
-    useEffect(() => {
-        setToken(getTokenFromCookies());
-    }, []);
-
-    // Effect to update the token state when the token changes
-    useEffect(() => {
-        // This effect runs whenever `token` changes (e.g., after sign out)
-        setToken(getTokenFromCookies());
-    }, [token]);
 
 
     return (
@@ -47,14 +23,9 @@ const Appbar = () => {
 
                 <div className="flex gap-4 items-center">
                     <Link className="font-semibold hover:text-blue-600" to={'/my-forms'}>My forms</Link>
-                    {token ? (
-                        <Button onClick={clickHandler} className="bg-red-600">Logout</Button>
-                    ) : (
-                        <>
-                            <Link className="font-semibold hover:text-blue-600" to={'/signin'}>Sign In</Link>
-                            <Link className="font-semibold hover:text-blue-600" to={'/signup'}>Sign Up</Link>
-                        </>
-                    )}
+                    <Button onClick={clickHandler} className="bg-red-600">Logout</Button>
+                    <Link className="font-semibold hover:text-blue-600" to={'/signin'}>Sign In</Link>
+                    <Link className="font-semibold hover:text-blue-600" to={'/signup'}>Sign Up</Link>
                 </div>
             </div>
         </div>
